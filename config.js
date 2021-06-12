3abc27ababe53344ff6142c5d77499e4099b9226
okkun510 <light.life510@hotmail.com>
2021-06-12 18:31:08 +0900

    [A0-4]Configの作成

---
+++ config.js
@@ -0,0 +1,6 @@
+require('dotenv').config();
+
+AUTH0_DOMAIN: process.env.AUTH0_DOMAIN
+AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID
+AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET
+AUTH0_ALLOW_DELETE: process.env.AUTH0_ALLOW_DELETE
